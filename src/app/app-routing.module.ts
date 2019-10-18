f0b538117da9f5d7761aac15c0b8d38ad3737891
nguyentruongiang <nguyentruonggiangptdq@gmail.com>
2019-10-18 23:29:26 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticationLayoutComponent } from './authentication-layout/authentication-layout.component';
+import { AuthenticationLoginComponent } from './authentication-login/authentication-login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
